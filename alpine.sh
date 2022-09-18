cdc271e994c12a481858a3cc703d82a7af37ed04
Shahrooz Abghari <shahroozabghari@gmail.com>
2022-09-18 10:38:01 +0200

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -103,11 +103,11 @@ log "Checking for latest NPM release"
 # Get latest version information for nginx-proxy-manager
 #runcmd 'wget $WGETOPT -O ./_latest_release $NPMURL/releases/latest'
 #_latest_version=$(basename $(cat ./_latest_release | grep -wo "NginxProxyManager/.*.tar.gz") .tar.gz | cut -d'v' -f2)
-#_latest_version=$(cat ./_latest_release | grep -Po '(?<=expanded_assets/v)[^"]+')
+_latest_version=2.9.18
 
 # Download nginx-proxy-manager source
-log "Downloading NPM v2.9.18"
-runcmd 'wget $WGETOPT -c $NPMURL/archive/refs/tags/v2.9.18.tar.gz -O - | tar -xz'
+log "Downloading NPM v$_latest_version"
+runcmd 'wget $WGETOPT -c $NPMURL/archive/refs/tags/v$_latest_version.tar.gz -O - | tar -xz'
 cd ./nginx-proxy-manager-$_latest_version
 
 log "Setting up enviroment"
